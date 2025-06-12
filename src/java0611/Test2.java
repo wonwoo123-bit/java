449be3aa67227139a4417707a8f61263b69a4763
Geonho2414 <161662681+Geonho2414@users.noreply.github.com>
2025-06-12 10:29:55 +0900

    tnwjd3

--- src/java0611/Test2.java
+++ src/java0611/Test2.java
@@ -1,8 +1,13 @@
 package java0611;
 
-public class Test1 {
+public class Test2 {
     public static void main(String[] args) {
         System.out.println("Hello World");
         System.out.println("Hi");
+        System.out.println(2 + 4);
+        System.out.println(2 + 4);
+        System.out.println("1+4+1+5"
+        );
+
     }
 }
---
+++ src/java0611/Test3.java
@@ -0,0 +1,13 @@
+package java0611;
+
+public class Test3 {
+    public static void main(String[] args) {
+        System.out.println("21");
+        //주석사용 연습
+        /*처음
+        ㅁㄴㅇ
+        ㅁㄴ으
+        */
+
+    }
+}
