19eab7ef72ec4bf01587a4abfe73ca87116e84c3
Geonho2414 <161662681+Geonho2414@users.noreply.github.com>
2025-06-17 17:26:02 +0900

    6.17예습

--- src/java0611/Test7.java
+++ src/java0611/Test7.java
@@ -2,11 +2,20 @@ package java0611;
 
 public class Test7 {
     public static void main(String[] args) {
+        if(1 > 3){
+            System.out.println("출력1");
+        }
+        else {
+            System.out.println("출력2");
+        }
         int a = 3;
-        int b = 5;
-        int c = 7;
-        System.out.println((a > b) ? "안녕하세요" : "반갑습니다");
-        System.out.println((a < b) ? (b > c) ? "타입 A" : "타입 B" : (b > c) ? "타입 C" : "타입 D");
+        if(a == 3){
+            System.out.println("출력3");
+        }
+        boolean b = false;
+        if(b){
+            System.out.println("출력4");
+        }
 
     }
 }
